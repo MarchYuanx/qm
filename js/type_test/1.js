b7275a75629d14b9d6a7fa2726e75ad9dabcb6ec
Yuanx1998 <592302815@qq.com>
2018-09-28 21:21:12 +0800

    js type & markman

---
+++ js/type_test/1.js
@@ -0,0 +1,39 @@
+var und = undefined; //简单类型
+var nul = null;
+var boo = true;
+var num = 1;
+var str = 'xys';
+// Sybmol
+
+// 复制数据类型 引用类型
+var obj = {a:1};
+// var obj1 ={};
+var func = function(){}
+
+
+
+//     for(var key in obj){
+//         obj1[key]=obj[key];
+//     }
+
+
+
+// console.log(obj);
+// console.log(obj1);
+
+// obj1.a = 2;
+
+// console.log('------------------');
+// console.log(obj);
+// console.log(obj1);
+
+
+
+
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof und);
+console.log(typeof boo);
+console.log(typeof nul);
+console.log(typeof func);
