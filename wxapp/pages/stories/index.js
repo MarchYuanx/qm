c2268651917d6f31d3870244c5cfd9e988ea6024
Yuanx1998 <592302815@qq.com>
2018-10-25 20:40:42 +0800

    wxapp update vehicles

--- wxapp/pages/stories/index.js
+++ wxapp/pages/stories/index.js
@@ -2,13 +2,31 @@ const app = getApp()
 
 Page({
     data:{
-
+        stories:[],
+        currentVid:null,
+        currentVideo:null
     },
     onLoad(options){
-        console.log('onload');
+        this.setData({
+            stories:app.globalData.stories
+        })
+        console.log('stories---page---load');
 
     },
     onReady:function(){
         
+    },
+    play(event){
+        if(this.data.currentVideo !== null){
+            this.data.currentVideo.pause();
+        }
+
+        const vid = event.currentTarget.dataset.vid;
+        const currentVideo = wx.createVideoContext(`${vid}`);
+        currentVideo.play();
+        this.setData({
+            currentVid: vid,
+            currentVideo,
+        })
     }
 });
