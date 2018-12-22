cef91c1fb2aac0b5dd006ff665809e675e97eb07
Yuanx1998 <592302815@qq.com>
2018-12-22 22:04:46 +0800

    progress

---
+++ react/progress/src/App.tsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import './App.css';
+import ProgressBar from './progressBar'
+
+
+class App extends React.Component {
+  public state = {
+    color: 'lightblue',
+    showInfo: true,
+    step: 5,
+    total: 24
+  }
+  public render() {
+   
+    return (
+      <div className="App">
+        <div>
+          <p>
+            <button onClick={this.decline}>-</button>
+            <button onClick={this.increase}>+</button>
+            <label>step:</label>
+            {this.state.step}
+          </p>
+        </div>
+        <ProgressBar
+          step={this.state.step}
+          total={this.state.total}
+          showInfo={this.state.showInfo}
+          color={this.state.color}/>
+      </div>
+    );
+  }
+
+  public decline = ()=>{
+    let step = this.state.step - 1 ;
+    if(step < 0){
+      step = 0;
+    }
+    this.setState({
+      step
+    })
+  }
+
+  public increase = ()=>{
+    let step = this.state.step + 1 ;
+    if(step > this.state.total){
+      step = this.state.total ;
+    }
+    this.setState({
+      step
+    })
+  }
+
+}
+
+export default App;
