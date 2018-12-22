fa901b2c1fc7b550db9118e2f6c862e940226b96
Yuanx1998 <592302815@qq.com>
2018-12-22 21:18:15 +0800

    redux-tutorial

---
+++ react/redux_tutorial/demo1.js
@@ -0,0 +1,11 @@
+var actionCreator = function(){
+  return {
+    type: 'AN_ACTION'
+  }
+}
+
+console.log(typeof actionCreator)
+//console.log(typeof actionCreator)
+console.log(actionCreator())
+
+// actionTypes = Constants
