1d9714b93ecfb4486f099a9318ad93ee18f08d8f
Yuanx1998 <592302815@qq.com>
2019-01-08 19:27:30 +0800

    goudanTV

---
+++ goudanTV/pages/welcome/welcome.js
@@ -0,0 +1,117 @@
+// pages/welcome/welcome.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    remind: '加载中',
+    angle: 0,
+    userInfo: {
+      avatarUrl: 'https://tvax3.sinaimg.cn/crop.8.7.322.322.180/8970ff1ely8frdenkcgutj209k09kdgw.jpg'
+    }
+  },
+  onConfirm(e) { // 点击允许
+    let dialogComponent = this.selectComponent('.wxc-dialog');
+    dialogComponent && dialogComponent.hide();
+    let userInfo = JSON.parse(e.detail.detail.rawData)
+    if (!userInfo) {
+      return;
+    }
+    this.setData({
+      userInfo: userInfo
+    })
+    wx.setStorageSync('userInfo', userInfo)
+  },
+  onCancel() { // 点击拒绝
+    let dialogComponent = this.selectComponent('.wxc-dialog');
+    dialogComponent && dialogComponent.hide();
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    wx.setNavigationBarTitle({
+      title: `狗蛋TV`
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {-
+    setTimeout(() => {
+      this.setData({
+        remind: ''
+      });
+    }, 1200);
+
+    wx.onAccelerometerChange((res) => {
+      let angle = -(res.x * 30).toFixed(1);
+      let maxAngle = 14;
+      if (angle > maxAngle) { angle = maxAngle; }
+      else if (angle < -1*maxAngle) { angle = -1*maxAngle; }
+      if (this.data.angle !== angle) {
+        this.setData({
+          angle: angle
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    let userInfo = wx.getStorageSync('userInfo')
+    let dialogComponent = this.selectComponent('.wxc-dialog');
+    if (!userInfo) {
+      dialogComponent && dialogComponent.show();
+    } else {
+      this.setData({
+        userInfo: userInfo
+      })
+      dialogComponent && dialogComponent.hide();
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  goToIndex() {
+    wx.switchTab({
+      url: '../music/music',
+    });
+  },
+})
